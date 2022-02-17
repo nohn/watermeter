a69c80ad67f7f32813749760884cc2f42fdd5f03
Sebastian Nohn <sebastian@nohn.net>
2022-02-17 04:50:10 +0100

    #15 - refactor analog gauges reader

---
+++ classes/Watermeter.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace nohn\Watermeter;
+
+class Watermeter
+{
+
+}
---
+++ tests/WatermeterReaderTest.php
@@ -0,0 +1,10 @@
+<?php
+
+
+use nohn\Watermeter\Reader;
+use PHPUnit\Framework\TestCase;
+
+class WatermeterReaderTest extends TestCase
+{
+
+}
