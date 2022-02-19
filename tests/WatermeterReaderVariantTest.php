47df77c403d427ed694875e2e0dd14e6499f6e0e
Sebastian Nohn <sebastian@nohn.net>
2022-02-19 08:14:04 +0100

    add test for ocr failing (ocred result is smaller)

--- tests/WatermeterReaderVariantTest.php
+++ tests/WatermeterReaderVariantTest.php
@@ -455,13 +455,86 @@ class WatermeterReaderVariantTest extends TestCase
                     'postprocessing' => false,
                 ),
             ),
-        'regular4_with_ocr_failing' =>
+        'regular4_with_ocr_passing' =>
+            array(
+                'lastValue' => 1189.9216,
+                'expectedValue' => 1189.9244,
+                'config' => array(
+                    'maxThreshold' => '0.2',
+                    'sourceImage' => __DIR__ . '/data/variants/4.jpg',
+                    'digitalDigits' =>
+                        array(
+                            2 =>
+                                array(
+                                    'x' => '189',
+                                    'y' => '47',
+                                    'width' => '36',
+                                    'height' => '58',
+                                ),
+                            3 =>
+                                array(
+                                    'x' => '249',
+                                    'y' => '47',
+                                    'width' => '36',
+                                    'height' => '58',
+                                ),
+                            4 =>
+                                array(
+                                    'x' => '304',
+                                    'y' => '47',
+                                    'width' => '36',
+                                    'height' => '58',
+                                ),
+                            5 =>
+                                array(
+                                    'x' => '364',
+                                    'y' => '47',
+                                    'width' => '36',
+                                    'height' => '58',
+                                ),
+                        ),
+                    'analogGauges' =>
+                        array(
+                            1 =>
+                                array(
+                                    'x' => '488',
+                                    'y' => '146',
+                                    'width' => '148',
+                                    'height' => '150',
+                                ),
+                            2 =>
+                                array(
+                                    'x' => '419',
+                                    'y' => '314',
+                                    'width' => '148',
+                                    'height' => '150',
+                                ),
+                            3 =>
+                                array(
+                                    'x' => '250',
+                                    'y' => '384',
+                                    'width' => '148',
+                                    'height' => '155',
+                                ),
+                            4 =>
+                                array(
+                                    'x' => '73',
+                                    'y' => '310',
+                                    'width' => '150',
+                                    'height' => '155',
+                                ),
+                        ),
+                    'logging' => false,
+                    'postprocessing' => false,
+                ),
+            ),
+        'regular5_with_ocr_failing' =>
             array(
                 'lastValue' => 1189.9216,
                 'expectedValue' => 1189.9216,
                 'config' => array(
                     'maxThreshold' => '0.2',
-                    'sourceImage' => __DIR__ . '/data/variants/4.jpg',
+                    'sourceImage' => __DIR__ . '/data/variants/5.jpg',
                     'digitalDigits' =>
                         array(
                             2 =>
