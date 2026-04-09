72d3ef6775228bf8313ff1899b67204223dca7cc
Sebastian Nohn <sebastian@nohn.net>
2026-04-09 13:48:52 +0200

    bring code too phpstan level 8

--- classes/Reader.php
+++ classes/Reader.php
@@ -20,7 +20,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * @author    Sebastian Nohn <sebastian@nohn.net>
- * @copyright 2022 Sebastian Nohn
+ * @copyright 2026 Sebastian Nohn
  * @license   http://www.fsf.org/licensing/licenses/agpl-3.0.html GNU Affero General Public License version 3.0
  */
 
@@ -121,7 +121,7 @@ class Reader extends Watermeter
             $numberOCR = '';
             $this->errors['TesseractOcrException'] = $e->getMessage();
         }
-        $numberDigital = preg_replace('/\s+/', '', $numberOCR);
+        $numberDigital = (string)preg_replace('/\s+/', '', $numberOCR);
         // There is TesseractOCR::digits(), but sometimes this will not convert a letter do a similar looking digit but completely ignore it. So we replace o with 0, I with 1 etc.
         $numberDigital = strtr($numberDigital, 'oOiIlzZsSBg', '00111225589');
         // $numberDigital = '00815';
--- public/index.php
+++ public/index.php
@@ -20,7 +20,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * @author    Sebastian Nohn <sebastian@nohn.net>
- * @copyright 2022 Sebastian Nohn
+ * @copyright 2026 Sebastian Nohn
  * @license   http://www.fsf.org/licensing/licenses/agpl-3.0.html GNU Affero General Public License version 3.0
  */
 
